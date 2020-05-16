87a0a36f63a76eb6203ad6f5a3e607da1c28d3c3
zebus3d <zebus3dream@gmail.com>
2020-05-17 00:08:33 +0200

    rellenada la lista de los integradores que creo q tiene

--- ui/properties.py
+++ ui/properties.py
@@ -6,8 +6,15 @@ def create_properties():
 
     # settigns comboboxes:
     integrator_types = (
-        ("direct", "Direct", "", 0),
-        ("path", "Path", "", 1),
+        ("direct_illumination", "Direct Illumination", "", 0),
+        ("path_tracer", "Path Tracer", "", 1),
+        ("photon_mapper", "Photon Mapper", "", 2),
+        ("simple_volumetric", "Simple Volumetric Path Tracer", "", 3),
+        ("extended_volumetric", "Extended Volumetric Path Tracer", "", 4),
+        ("energy_redistribution", "Energy Redistribution PT", "", 5),
+        ("primary_sample_space_mtl", "Primary Sample Space MTL", "", 6),
+        ("stochastic_progresive", "Stochastic Progresive Phoyon Mapping", "", 7),
+        ("ambient_occlusion", "Ambient Occlusion", "", 8),
     )
 
     # setup comboboxes:
